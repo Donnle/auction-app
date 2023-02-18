85036e45f08e82553a22b9bb9fbc6f548fa8602d
VadimParpolita <vadim.parpolita@theraven.tech>
2023-02-18 23:09:34 +0200

    AUC-7:ISSUE: [Backend] Fix pagination

--- api/services/Market-service.js
+++ api/services/Market-service.js
@@ -1,15 +1,29 @@
 const Product = require('../models/Product-model');
 
 class MarketService {
-  async getProducts(page, count, query) {
+  async getProducts(page, count, query = '') {
     const countSkipProducts = (page - 1) * count;
-    const products = await Product.find({
-      title: { '$regex': query || '' },
-      description: { '$regex': query || '' },
-    }).skip(countSkipProducts).limit(count);
 
-    const countProducts = await Product.count();
-    const countPages = Math.ceil(countProducts / count);
+    let products;
+    let countProducts;
+    let countPages;
+
+    if (query) {
+      products = await Product.find({
+        $or: [
+          { title: { '$regex': query } },
+          { description: { '$regex': query } },
+        ],
+      }).skip(countSkipProducts).limit(count);
+      countProducts = await Product.count();
+      countPages = Math.ceil(countProducts / count);
+    } else {
+      const allProducts = await Product.find();
+
+      products = allProducts.slice(countSkipProducts, page * count);
+      countProducts = allProducts.length;
+      countPages = Math.ceil(countProducts / count);
+    }
 
     return {
       products,
