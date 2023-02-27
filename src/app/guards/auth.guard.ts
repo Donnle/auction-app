02390efdd2b0b2d882ba6fcd709cf782695c343b
VadimParpolita <vadim.parpolita@theraven.tech>
2023-02-27 09:10:15 +0200

    AUC-16:TASK: [Frontend][Backend] Refactor code

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -7,12 +7,12 @@ import { AuthService } from '../services/auth.service';
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  isUserAuthorized: boolean;
+  isLoggedIn: boolean;
 
   constructor(private router: Router, private authService: AuthService) {
     this.authService.isLoggedIn$.subscribe({
       next: (isUserAuthorized: boolean) => {
-        this.isUserAuthorized = isUserAuthorized;
+        this.isLoggedIn = isUserAuthorized;
       },
     });
   }
@@ -20,9 +20,9 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (!this.isUserAuthorized) {
+    if (!this.isLoggedIn) {
       this.router.navigate(['/']);
     }
-    return this.isUserAuthorized;
+    return this.isLoggedIn;
   }
 }
